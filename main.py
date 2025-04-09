3f080f541ec3e702ed92dd67d2728d05d7f4888e
vachan-maker <vachan@tutanota.com>
2025-04-09 22:17:13 +0530

    Use a modal for the todo item for input validation

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from models import Todo
 app = FastAPI()
 
+todos = []
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# Get all todos
+@app.get("/todos")
+async def get_todos():
+    return {"todos":todos}
+
+
+# Get single todos
+@app.post("/todos")
+async def create_todos(todo: Todo):
+    todos.append(todo)
+    return {"message":"Todo has been added"}
+# Update a todo
+
+# Delete a todo
