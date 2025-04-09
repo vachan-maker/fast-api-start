144475c391c3180e364133eeb86c5d628f68433e
vachan-maker <vachan@tutanota.com>
2025-04-09 23:16:03 +0530

    Added route for getting individual todo

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ async def get_todos():
     return {"todos":todos}
 
 
-# Get single todos
+# create a todo
 @app.post("/todos")
 async def create_todos(todo: Todo):
     todos.append(todo)
     return {"message":"Todo has been added"}
+# Get a todo
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id:int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo":todo}
+    return {"message":"Todo not found"}
 # Update a todo
 
 # Delete a todo
